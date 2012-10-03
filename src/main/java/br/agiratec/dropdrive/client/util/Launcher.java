b4eb5445a1945d125ea8be17db5598cdc0656f4f
Rodrgio Bussolo <rbussolo91@gmail.com>
2012-10-03 17:55:20 -0400

    Launcher correto.

--- src/main/java/br/agiratec/dropdrive/client/util/Launcher.java
+++ src/main/java/br/agiratec/dropdrive/client/util/Launcher.java
@@ -3,17 +3,20 @@ package br.agiratec.dropdrive.client.util;
 import javax.swing.SwingUtilities;
 
 import br.agiratec.dropdrive.client.frame.LoginView;
-import br.agiratec.dropdrive.client.socket.SocketReceiver;
+import br.agiratec.dropdrive.client.frame.PrimaryAspect;
+import br.agiratec.dropdrive.client.service.DropDriveServiceOrchestrator;
+import br.agiratec.dropdrive.client.service.SocketReceiver;
 
 public class Launcher {
 	
 	public Launcher(){
 		SwingUtilities.invokeLater(new Runnable() {			
 			public void run() {
-				new LoginView();				
+				new PrimaryAspect();				
 			}
 		});		
-		new SocketReceiver();		
+		
+		DropDriveServiceOrchestrator.getInstance().initServices();
 	}
 	
 	public static void main(String[] args) {
